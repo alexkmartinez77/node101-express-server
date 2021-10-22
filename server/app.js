a6e408a1c01fa07809052341f4ec9be099500e1c
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-22 13:36:18 -0700

    Creates Express server with 2 routes, which parses JSON file and sends it along with 200 response.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
-
-
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var jsonObject = require('./data.json');
 // create your express server below
-var app;
+var app = express();
 
-// add your routes and middleware below
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json())
 
+//routes
+app.get('/', function(req,res){
+  res.status(200).send('New express Server');
+
+});
+
+app.get('/data', function(req,res){
+  res.status(200).send(jsonObject);
+});
 // finally export the express application
 module.exports = app;
