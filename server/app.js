d368bd9460dc102fdd62fcc545f4d883bb1b92c9
Alex Martinez <alexkmartinez77@gmail.com>
2022-04-27 16:21:23 -0700

    Removes superfluous code.

--- server/app.js
+++ server/app.js
@@ -1,23 +1,21 @@
 // import files and packages up here
 var express = require('express');
 var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var jsonObject = require('./data.json');
+var data = require('./data.json');
 // create your express server below
 var app = express();
 
 //middleware
 app.use(morgan('dev'));
-app.use(bodyParser.json())
 
 //routes
 app.get('/', function(req,res){
   res.status(200).send('New express Server');
-
 });
 
 app.get('/data', function(req,res){
-  res.status(200).send(jsonObject);
+  res.status(200).send(data);
 });
+
 // finally export the express application
 module.exports = app;
